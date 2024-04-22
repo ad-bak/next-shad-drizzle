b963ecbec6fb17dd8939da238d30c0310a4daa1a
Adnan <adbak.dev@gmail.com>
2024-04-22 23:45:13 +0200

    db setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,30 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
-export default function HomePage() {
-  const mockUrls = [
-    "https://www.wowmakers.com/static/Video-thumbnail-e743f3689ca0c0bac8faab39023da37f.jpeg",
-    "https://static-cse.canva.com/blob/1436087/1600w-wK95f3XNRaM.jpg",
-    "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/thumbnail-background-youtube-2023-design-template-d1ba8caa87a7e45a222132372cd336a7_screen.jpg?ts=1674608286",
-    "https://ergonotes.com/wp-content/uploads/2022/11/Find-YouTube-Thumbnail-Source.jpg",
-  ];
+const mockUrls = [
+  "https://www.wowmakers.com/static/Video-thumbnail-e743f3689ca0c0bac8faab39023da37f.jpeg",
+  "https://static-cse.canva.com/blob/1436087/1600w-wK95f3XNRaM.jpg",
+  "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/thumbnail-background-youtube-2023-design-template-d1ba8caa87a7e45a222132372cd336a7_screen.jpg?ts=1674608286",
+  "https://ergonotes.com/wp-content/uploads/2022/11/Find-YouTube-Thumbnail-Source.jpg",
+];
 
-  const mockImages = mockUrls.map((url, index) => ({
-    id: index + 1,
-    url,
-  }));
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+  console.log(posts);
 
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
         {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
-          <div className="h-200 w-48 p-4" key={image.id + index}>
+          <div className="h-200 w-48 p-4" key={image.id + "-" + index}>
             <img src={image.url} className="h-full" />
           </div>
         ))}
