33c8566918aa3aa9a8f18b9ac8c1768e22127432
Adnan <adbak.dev@gmail.com>
2024-04-23 22:03:07 +0200

    add images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,31 +2,21 @@ import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://www.wowmakers.com/static/Video-thumbnail-e743f3689ca0c0bac8faab39023da37f.jpeg",
-  "https://static-cse.canva.com/blob/1436087/1600w-wK95f3XNRaM.jpg",
-  "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/thumbnail-background-youtube-2023-design-template-d1ba8caa87a7e45a222132372cd336a7_screen.jpg?ts=1674608286",
-  "https://ergonotes.com/wp-content/uploads/2022/11/Find-YouTube-Thumbnail-Source.jpg",
-];
-
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
-
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
-  console.log(posts);
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
 
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {posts.map((post) => (
-          <div key={post.id}>{post.name}</div>
-        ))}
-        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
-          <div className="h-200 w-48 p-4" key={image.id + "-" + index}>
+        {[...images].map((image, index) => (
+          <div
+            className="flex h-52 w-60 flex-col p-4"
+            key={image.id + "-" + index}
+          >
             <img src={image.url} className="h-full" />
+            <div>{image.name}</div>
           </div>
         ))}
       </div>
