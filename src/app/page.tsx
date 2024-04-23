3aa66fa14eaf0d21f70d20727ee0b4e59b689cb1
Adnan <adbak.dev@gmail.com>
2024-04-23 22:52:03 +0200

    use auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,38 @@
+import { SignIn, SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
+  return (
+    <div className="flex flex-wrap gap-4">
+      {[...images].map((image, index) => (
+        <div
+          className="flex h-52 w-60 flex-col p-4"
+          key={image.id + "-" + index}
+        >
+          <img src={image.url} className="h-full" />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
 
+export default async function HomePage() {
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {[...images].map((image, index) => (
-          <div
-            className="flex h-52 w-60 flex-col p-4"
-            key={image.id + "-" + index}
-          >
-            <img src={image.url} className="h-full" />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">
+          You must be signed in to view this page.
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
