0661ed78c9992aad8d083cd559a6dd1c9177e2cb
Adnan <adbak.dev@gmail.com>
2024-04-27 20:46:17 +0200

    svg

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,37 +1,20 @@
 "use client";
 
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-import { useRouter } from "next/navigation";
-import { UploadButton } from "~/utils/uploadthing";
+import { SimpleUploadButton } from "./upload-button";
 
 export function TopNav() {
-  const router = useRouter();
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
       <div>Gallery</div>
-      <div className="flex flex-row">
+      <div className="flex flex-row items-center gap-4">
         <SignedOut>
           <SignInButton />
         </SignedOut>
-        <div className="flex flex-row gap-4">
-          <SignedIn>
-            <UploadButton
-              onClientUploadComplete={() => {
-                router.refresh();
-              }}
-              endpoint="imageUploader"
-              className="text-lg"
-              content={{
-                button: "Upload",
-              }}
-              appearance={{
-                button: "bg-rose-600 text-white px-4 py-2 rounded",
-                container: "flex flex-row gap-4",
-              }}
-            />
-            <UserButton />
-          </SignedIn>
-        </div>
+        <SignedIn>
+          <SimpleUploadButton />
+          <UserButton />
+        </SignedIn>
       </div>
     </nav>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,6 @@ import Image from "next/image";
 import Link from "next/link";
 import { getMyImages } from "~/server/queries";
 
-export const dynamic = "force-dynamic";
-
 async function Images() {
   const images = await getMyImages();
   return (
