99b4eaf94fcfd0d4546b4db6d901856e3fe92592
Adnan <adbak.dev@gmail.com>
2024-04-27 19:56:34 +0200

    grid

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,15 +31,27 @@ export default function RootLayout({
       <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
       <html lang="en">
         <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
-        <body className={`font-sans ${inter.variable} flex flex-col gap-4`}>
-          <div>
+        <body className={`font-sans ${inter.variable}`}>
+          <div className="grid h-screen grid-rows-[auto,1fr]">
             <TopNav />
-            <main>{children}</main>
-            {modal}
+            <main className="overflow-y-scroll ">{children}</main>
           </div>
+          {modal}
           <div id="modal-root" />
         </body>
       </html>
     </ClerkProvider>
   );
+}
+
+{
+  /* <body className={`font-sans ${inter.variable} dark`}>
+  <div className="grid h-screen grid-rows-[auto,1fr]">
+    <TopNav />
+    <main className="overflow-y-scroll">{children}</main>
+    {modal}
+  </div>
+  <div id="modal-root" />
+  <Toaster />
+</body>; */
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export const dynamic = "force-dynamic";
 async function Images() {
   const images = await getMyImages();
   return (
-    <div className="flex flex-wrap justify-center gap-4">
+    <div className="flex flex-wrap justify-center gap-4 p-4">
       {[...images].map((image) => (
         <div className="flex h-48 w-60 flex-col" key={image.id}>
           <Link href={`/img/${image.id}`}>
