c70f2c510a54eb762f2ee95124f66b492811e60f
Adnan <adbak.dev@gmail.com>
2024-04-27 21:29:33 +0200

    add sonner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { TopNav } from "./_components/topnav";
 import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
 import { extractRouterConfig } from "uploadthing/server";
 import { ourFileRouter } from "./api/uploadthing/core";
+import { Toaster } from "sonner";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -31,27 +32,16 @@ export default function RootLayout({
       <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
       <html lang="en">
         <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
-        <body className={`font-sans ${inter.variable}`}>
+        <body className={`font-sans ${inter.variable} dark`}>
           <div className="grid h-screen grid-rows-[auto,1fr]">
             <TopNav />
             <main className="overflow-y-scroll ">{children}</main>
           </div>
           {modal}
           <div id="modal-root" />
+          <Toaster />
         </body>
       </html>
     </ClerkProvider>
   );
-}
-
-{
-  /* <body className={`font-sans ${inter.variable} dark`}>
-  <div className="grid h-screen grid-rows-[auto,1fr]">
-    <TopNav />
-    <main className="overflow-y-scroll">{children}</main>
-    {modal}
-  </div>
-  <div id="modal-root" />
-  <Toaster />
-</body>; */
 }
